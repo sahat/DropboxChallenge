767f3665ae4b6f9db1cd5db04080107de40233b6
Sahat Yalkabov <sakhat@gmail.com>
2014-07-19 04:43:39 -0400

    hide copyright and login when after signing in

--- js/app.js
+++ js/app.js
@@ -7,23 +7,39 @@
 // TODO: Score and Achievements
 
 (function() {
+
+  // Initialize LinkedIn SDK
   IN.init({
     api_key: '75z17ew9n8c2pm',
     authorize: true
   });
 
-  var loginBtn = document.querySelector('.btn-linkedin');
-  loginBtn.addEventListener('click', function() {
-    console.log('click')
-//  IN.UI.Authorize().place();
-//  IN.Event.on(IN, 'auth', function() {
-//    var source = document.querySelector("#entry-template").innerHTML;
-//    document.querySelector('#main').innerHTML = _.template(source, { name: 'pebbles' });
-//  });
-  });
+  // DOM Elements
+  var main = document.querySelector('#main');
+  var title = document.querySelector('.title');
+  var centered = document.querySelector('.centered');
+  var loginButton = document.querySelector('.btn-linkedin');
+  var copyright = document.querySelector('.copyright');
 
+
+  // Event Listeners
+  loginButton.addEventListener('click', function(e) {
+    console.log('click');
+
+    centered.style.marginTop = '0';
+    centered.style.top = '0';
+    loginButton.style.display = 'none';
+    copyright.style.display = 'none';
+
+  });
 
   function onLinkedInLoad() {
 
   }
 })();
+
+//  IN.UI.Authorize().place();
+//  IN.Event.on(IN, 'auth', function() {
+//    var source = document.querySelector("#entry-template").innerHTML;
+//    document.querySelector('#main').innerHTML = _.template(source, { name: 'pebbles' });
+//  });
