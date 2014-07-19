aaf5f559207ef21b322252a79bf17c7ae480ddd3
Sahat Yalkabov <sakhat@gmail.com>
2014-07-19 01:18:21 -0400

    open linkedin login popup when clicking on a sign-in button

--- js/app.js
+++ js/app.js
@@ -1,20 +1,28 @@
 /**
  * Created by Sahat on 7/18/2014.
  */
-
+// TODO: code climate + tests
 // TODO: Linkedin client auth
 // TODO: Linkedin API call to /connections
 // TODO: When connections are loaded, replace DOM with grid
 // TODO: Add logout button
 // TODO: Reset game
 // TODO: Score and Achievements
-// TODO: font-family: "Abril Fatface", serif;
-// TODO:'Lato', Arial, sans-serif;
-// TODO:
-// TODO:
-// TODO:
-// TODO:
-// TODO:
-// TODO:
-// TODO:
-// TODO:
+
+IN.init({
+  api_key: '75z17ew9n8c2pm',
+  authorize: true
+});
+
+var loginBtn = document.querySelector('.btn-linkedin');
+loginBtn.addEventListener('click', function() {
+  IN.UI.Authorize().place();
+  IN.Event.on(IN, 'auth', function() {
+
+  });
+});
+
+
+function onLinkedInLoad() {
+
+}
